f48ce05fe031908b040e7a75b24ceb97e81c309c
Scott Davis <scott@goodjob.io>
2023-09-25 14:21:06 -0600

    initial commit

---
+++ tests/product_page_add_item.spec.js
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test('Product Page Add to Basket', async ({ page }) => {
+  await page.goto('http:localhost:2221');
+
+  const addToBasketButton = page.getByRole('button', { name: 'Add to Basket' }).first();
+  await addToBasketButton.waitFor();
+  await addToBasketButton.click();
+
+  await page.pause();
+});
