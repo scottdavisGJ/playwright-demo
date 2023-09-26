a94575c42b2242b42c892286b6c3b7703869d9da
Scott Davis <scott@goodjob.io>
2023-09-26 14:52:45 -0600

    Add check for count before and after adding items to basket

--- page-objects/ProductsPage.js
+++ page-objects/ProductsPage.js
@@ -4,17 +4,28 @@ export class ProductsPage {
   constructor(page) {
     this.page = page;
     this.addButtons = page.locator('[data-qa="product-button"]');
+
+    this.basketCounter = page.locator('[data-qa="header-basket-count"]');
   }
 
   visit = async () => {
     await this.page.goto('/');
   };
 
+  getBasketCount = async () => {
+    await this.basketCounter.waitFor();
+    const text = await this.basketCounter.innerText();
+    return parseInt(text, 10);
+  };
+
   addProductToBasket = async (index) => {
     const specificAddButton = this.addButtons.nth(index);
     await specificAddButton.waitFor();
     await expect(specificAddButton).toHaveText('Add to Basket');
+    const basketCountBeforeAdding = await this.getBasketCount();
     await specificAddButton.click();
     await expect(specificAddButton).toHaveText('Remove from Basket');
+    const basketCountAfterAdding = await this.getBasketCount();
+    expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding);
   };
 }
