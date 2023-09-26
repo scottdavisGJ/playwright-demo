7505c0d029e980657ea0f082358135e2c3b470ff
Scott Davis <scott@goodjob.io>
2023-09-26 15:43:22 -0600

    Remove cheapest item from basket, Check count of items after removing the cheapest

---
+++ page-objects/Checkout.js
@@ -0,0 +1,33 @@
+import { expect } from '@playwright/test';
+
+export class Checkout {
+  constructor(page) {
+    this.page = page;
+
+    this.basketCards = page.locator('[data-qa="basket-card"]');
+    this.basketItemPrice = page.locator('[data-qa="basket-item-price"]');
+    this.basketItemRemoveButton = page.locator('[data-qa="basket-card-remove-item"]');
+  }
+
+  removeCheapestProduct = async () => {
+    await this.basketCards.first().waitFor();
+    const itemsBeforeRemoval = await this.basketCards.count();
+
+    await this.basketItemPrice.first().waitFor();
+    const allPriceTexts = await this.basketItemPrice.allInnerTexts();
+    const justNumbers = allPriceTexts.map((element) => {
+      const withoutDollarSign = element.replace('$', '');
+      return parseInt(withoutDollarSign, 10);
+    });
+
+    const smallestPrice = Math.min(justNumbers);
+    const smallestPriceIndex = justNumbers.indexOf(smallestPrice);
+    const specificRemoveButton = this.basketItemRemoveButton.nth(smallestPriceIndex);
+
+    await specificRemoveButton.waitFor();
+    await specificRemoveButton.click();
+    await expect(this.basketCards).toHaveCount(itemsBeforeRemoval - 1);
+
+    await this.page.pause();
+  };
+}
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 import { ProductsPage } from '../page-objects/ProductsPage.js';
 import { Navigation } from '../page-objects/Navigation.js';
+import { Checkout } from '../page-objects/Checkout.js';
 
 test.only('New user full end-to-end test journey', async ({ page }) => {
   const productsPage = new ProductsPage(page);
@@ -10,5 +11,9 @@ test.only('New user full end-to-end test journey', async ({ page }) => {
   await productsPage.addProductToBasket(2);
   const navigation = new Navigation(page);
   await navigation.goToCheckout();
-  await page.pause();
+
+  const checkout = new Checkout(page);
+  await checkout.removeCheapestProduct();
+
+  // await page.pause();
 });
